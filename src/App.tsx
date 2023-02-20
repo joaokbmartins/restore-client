8876dd27e500753acc7665ee9a17214a62db3988
João Martins <martins.joao1909@gmail.com>
2023-02-20 07:57:04 -0300

    chore: fetching api data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+export interface IProduct {
+  id: number,
+  type: string,
+  name: string,
+  price: number,
+  brand: string,
+  pictureUrl: string,
+  description: string,
+  quantityInStock: number,
+}
 
 function App() {
+  const [products, setProducts] = useState([] as IProduct[]);
+
+  function fetchData() {
+    return fetch("http://localhost:5000/api/v1/Products")
+      .then(response => response.json())
+      .then((json: IProduct[]) => json);
+  }
+
+  useEffect(() => {
+    fetchData()
+      .then((data: IProduct[]) => setProducts(data));
+  }, []);
+
+  function addProduct() {
+    setProducts(
+      (previousState: IProduct[]) =>
+        [
+          ...previousState,
+          {
+            "id": previousState.slice(-1)[0].id + 1,
+            "price": 0,
+            "quantityInStock": 0,
+            "brand": 'string',
+            "description": 'string',
+            "name": 'string',
+            "pictureUrl": 'string',
+            "type": 'string',
+          }
+        ]
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ color: 'blue' }}>
+      <h1>Re-Store</h1>
+
+      <button onClick={addProduct}>Add Product</button>
+
+      <ul>
+        {
+          products.map(
+            (product, index) => (
+              <li key={index}>{product.id} - {product.price} - {product.quantityInStock} - {product.brand} - {product.description} - {product.name} - {product.pictureUrl} - {product.type}</li>
+            )
+          )
+        }
+      </ul>
     </div>
   );
 }
